f1886a5fe1f9d1119e28e858ad7dfda82552790e
CecDor <150656709+CecDorWEB@users.noreply.github.com>
2025-01-25 13:10:27 +0100

    Add router and organization of my first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LogInComponent } from './component/log-in/log-in.component';
+import { RegisterComponent } from './component/register/register.component';
+
+const routes: Routes = [
+  { path: 'logIn', component: LogInComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LogInComponent } from './log-in/log-in.component';
-import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './component/log-in/log-in.component';
+import { RegisterComponent } from './component/register/register.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LogInComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    RegisterComponent,
+    NavbarComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
